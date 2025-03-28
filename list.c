11639817fc04a26b085d6718c3a731fb139e348b
Your Name <you@example.com>
2025-03-27 21:08:49 -0300

    new try

--- list.c
+++ list.c
@@ -88,7 +88,6 @@ void pushBack(List * list, void * data) {
 
 void pushCurrent(List * list, void * data) {
     Node* newNode = createNode(data);
-    newNode->data = data;
     newNode->prev = list->current;
     newNode->next = list->current->next;
 
